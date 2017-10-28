73ec4ee7278e39a8eb5f159c0dc98088a415bff5
TheAnarchoX <jimdvries@gmail.com>
2017-10-28 22:10:18 +0200

    Description fix

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="author" content="Jim de Vries"/>
-    <meta name="description" content="Starfish"/>
+    <meta name="description" content="HogerLager"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
      <link rel="stylesheet" href="css/style.css">
